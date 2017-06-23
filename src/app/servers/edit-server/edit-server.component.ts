8f467604c148ae5fa9dea425af620ac8be18f438
Quy Tran <quytran288@gmail.com>
2017-06-23 15:29:14 -0500

    Get id from passed parameter and use it for getting data for server

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: 'app-edit-server',
@@ -20,9 +20,18 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.fragment);
     this.route.queryParams.subscribe();
     this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+        this.serverName = this.server.name;
+        this.serverStatus = this.server.status;
+      }
+    );
+    const id: number = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    
   }
 
   onUpdateServer() {
