086aeb0b9ed707e7419159ec38c5aa27d6694989
Quy Tran <quytran288@gmail.com>
2017-06-24 16:39:09 -0500

    Protect route using CanActivate or CanActivateChild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ServersComponent } from "app/servers/servers.component";
 import { ServerComponent } from "app/servers/server/server.component";
 import { EditServerComponent } from "app/servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "app/page-not-found/page-not-found.component";
+import { AuthGuard } from "app/auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
         ]
     },
     {
-        path: 'servers', component: ServersComponent, children: [
+        path: 'servers', /*canActivate: [AuthGuard]*/ canActivateChild: [AuthGuard], component: ServersComponent, children: [
             { path: ':id', component: ServerComponent },
             { path: ':id/edit', component: EditServerComponent }
         ]
